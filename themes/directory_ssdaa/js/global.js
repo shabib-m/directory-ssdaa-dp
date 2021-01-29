4c7092464a19bbf429cb40699d68913eed0063f0
shabib-m <abngeath@gmail.com>
2021-01-30 00:46:41 +0300

    add new theme bootstrap_barrio and directory_ssdaa

---
+++ themes/directory_ssdaa/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.directory_ssdaa = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
